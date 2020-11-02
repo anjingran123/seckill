e70836d8cc2244b60dff839c0cea857627b10688
anjingran_v@sina.com <93tiandaochouqin>
2020-11-02 14:59:55 +0800

    1234

--- src/main/java/cn/codingxiaxw/dao/SeckillDao.java
+++ src/main/java/cn/codingxiaxw/dao/SeckillDao.java
@@ -29,7 +29,7 @@ public interface SeckillDao
     Seckill queryById(long seckillId);
 
     /**
-     * 根据偏移量查询秒杀商品列表111
+     * 根据偏移量查询秒杀商品列表1
      * @param offset
      * @param limit
      * @return
